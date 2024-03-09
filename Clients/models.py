09bc0ad6bb47883b606ca6d417bcc734f8543790
Atharva <Atharvapawar34s@gmail.com>
2024-03-09 13:46:42 +0530

    third commit

--- Clients/models.py
+++ Clients/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
+
 # Create your models here.
+class Plan(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+class Member(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=15)
+    email = models.EmailField(unique=True)
+    age = models.PositiveIntegerField()
+    gender = models.CharField(max_length=10)
+    plan = models.ForeignKey(Plan, on_delete=models.SET_NULL, null=True)
+    enrolled_on = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name}"
+
