319d9381f02488198a6754473a6b076b2e662419
Atharva <Atharvapawar34s@gmail.com>
2024-03-15 13:19:42 +0530

    add date at member and enquirypage

--- Clients/models.py
+++ Clients/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser, Group, Permission
+from django.contrib.auth.models import *
 
 # Create your models here.
 class CustomUser(AbstractUser):
